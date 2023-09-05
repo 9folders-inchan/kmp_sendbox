fa39197fab2bbad3e2e62445ad2ee2a131d1a134
runner <runner@Mac-1693908327126.local>
2023-09-05 10:12:01 +0000

    KMM SPM package release for 0.1.12

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://maven.pkg.github.com/9folders-inchan/kmp_sendbox/com/9folders/kmp/sendbox/allshared-kmmbridge/0.1.9/allshared-kmmbridge-0.1.9.zip"
-let remoteKotlinChecksum = "d5cb5f446c71e2b47a1a8e799309113169ae0de3cecb6a79e58f774b5da477b4"
+let remoteKotlinUrl = "https://maven.pkg.github.com/9folders-inchan/kmp_sendbox/com/9folders/kmp/sendbox/allshared-kmmbridge/0.1.12/allshared-kmmbridge-0.1.12.zip"
+let remoteKotlinChecksum = "dffeb33865a6f3105e29824ac8e0221004861e49153da505013926ae9746b5e7"
 let packageName = "allshared"
 // END KMMBRIDGE BLOCK
 
