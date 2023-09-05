aa539c3f21a017e58a91e3bd6ec3fe7a9e83cbee
inchan <inchan.kang@9folders.com>
2023-09-05 20:13:55 +0900

    KMM SPM package release for 0.1.22

--- Package.swift
+++ Package.swift
@@ -2,12 +2,13 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://maven.pkg.github.com/9folders-inchan/kmp_sendbox/com/9folders/kmp/sendbox/allshared-kmmbridge/0.1.20/allshared-kmmbridge-0.1.20.zip"
+let remoteKotlinUrl = "https://maven.pkg.github.com/9folders-inchan/kmp_sendbox/com/9folders/kmp/sendbox/allshared-kmmbridge/0.1.22/allshared-kmmbridge-0.1.22.zip"
 let remoteKotlinChecksum = "d5cb5f446c71e2b47a1a8e799309113169ae0de3cecb6a79e58f774b5da477b4"
 let packageName = "allshared"
 // END KMMBRIDGE BLOCK
-let target = "\(packageName)target"
-let wrapper = "\(packageName)wrapper"
+
+let packageTarget = "\(packageName)target"
+let packageWrapper = "\(packageName)wrapper"
 
 let package = Package(
     name: packageName,
@@ -17,19 +18,19 @@ let package = Package(
     products: [
         .library(
             name: packageName,
-            targets: [target]
+            targets: [packageTarget, packageName]
         )
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
     ],
     targets: [
-        .target(name: target,
+        .target(name: packageTarget,
                 dependencies: [
-                    .target(name: wrapper)
+                    .target(name: packageWrapper)
                 ]
                ),
-        .target(name: wrapper,
+        .target(name: packageWrapper,
                 dependencies: [
                     .target(name: packageName),
                     .product(name: "Alamofire", package: "Alamofire")
