e3469517b1fb2d557e6cbeb8e01047a209536cd1
inchan <inchan.kang@9folders.com>
2023-09-05 18:18:11 +0900

    [*] test commit

--- Package.swift
+++ Package.swift
@@ -1,11 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://maven.pkg.github.com/9folders-inchan/kmp_sendbox/com/9folders/kmp/sendbox/allshared-kmmbridge/0.1.3/allshared-kmmbridge-0.1.3.zip"
-let remoteKotlinChecksum = "78915a034752346a7b886086ee7e9860afd0c7d2d81886b2f998fcccd0f45677"
 let packageName = "allshared"
-// END KMMBRIDGE BLOCK
 
 let package = Package(
     name: packageName,
@@ -18,12 +14,15 @@ let package = Package(
             targets: [packageName]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0")),
+    ],
     targets: [
         .binaryTarget(
             name: packageName,
-            url: remoteKotlinUrl,
-            checksum: remoteKotlinChecksum
+            path: "./allshared/build/XCFrameworks/debug/\(packageName).xcframework"
         )
         ,
     ]
+
 )
