9b53bd8cf8065a9d6483c5223c2cc92b4405d11b
inchan <inchan.kang@9folders.com>
2023-09-05 19:03:52 +0900

    KMM SPM package release for 0.1.9

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://maven.pkg.github.com/9folders-inchan/kmp_sendbox/com/9folders/kmp/sendbox/allshared-kmmbridge/0.1.8/allshared-kmmbridge-0.1.8.zip"
+let remoteKotlinUrl = "https://maven.pkg.github.com/9folders-inchan/kmp_sendbox/com/9folders/kmp/sendbox/allshared-kmmbridge/0.1.9/allshared-kmmbridge-0.1.9.zip"
 let remoteKotlinChecksum = "d5cb5f446c71e2b47a1a8e799309113169ae0de3cecb6a79e58f774b5da477b4"
 let packageName = "allshared"
 // END KMMBRIDGE BLOCK
